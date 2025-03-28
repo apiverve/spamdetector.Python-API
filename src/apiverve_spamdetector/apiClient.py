a12ea7bea016e757d427091bc154f0f68539b400
APIVerve Team <hello@apiverve.com>
2025-03-28 17:50:36 -0500

    Bug Fixes

--- src/apiverve_spamdetector/apiClient.py
+++ src/apiverve_spamdetector/apiClient.py
@@ -10,10 +10,10 @@ class SpamdetectorAPIClient:
         
         self.api_key = api_key
         self.secure = secure
-        self.base_url = 'https://api.apiverve.com/v1.0.9/spamdetector'
+        self.base_url = 'https://api.apiverve.com/v1/spamdetector'
         self.headers = {
             'x-api-key': self.api_key,
-            'auth-mode': 'pypi'
+            'auth-mode': 'pypi-package'
         }
 
     def execute(self, params=None):
